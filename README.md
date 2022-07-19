593209f91ada60c7efe81627c5efe24cd06cd8dd
yanukadeneth99 <yanukadeneth99@gmail.com>
2022-07-19 19:26:38 +0530

    Working Project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Using Ceramic Web3
+
+- Create Account and Change the name using Ceramic
+
+### Education
+
+[LearnWeb3DAO](https://www.learnweb3.io/tracks/junior/ceramic-network)
---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "@self.id/react";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider client={{ ceramic: "testnet-clay" }}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
+
+export default MyApp;
